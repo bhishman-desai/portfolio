4d1c7f7d3fa504c82962eca1b33fd83d04e66ddd
Bhishman Desai <desaibhishman1@gmail.com>
2024-12-10 15:07:12 -0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
           href="/CV.pdf"
           download
         >
-          Download CV{" "}
+          Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
